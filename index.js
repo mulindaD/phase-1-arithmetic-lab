6f4875f632b155b57f458692bfa3737074366e23
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-06-27 13:50:11 +0300

    Complete arithmentic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random())
+
+let num3 = 7
+let num4 = 25
+const mod = num4 % num3
+
+const max = Math.max(1,2,3,20)
+
